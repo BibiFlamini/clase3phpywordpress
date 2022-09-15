66aed038c2ac3e33ca4285d37d7486635da36264
Bibiana <lucerina23@gmail.com>
2022-09-15 16:29:08 -0300

    agregando funcion post al formulario

--- contacto.php
+++ contacto.php
@@ -7,27 +7,34 @@
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="nombre">Nombre:</label>
-                <input type="text" class="form-control" id="nombre" placeholder="">
+                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre">
               </div>
               <br>
               <div class="form-group col-md-6">
                 <label for="apellido">Apellido:</label>
-                <input type="text" class="form-control" id="apellido" placeholder="">
+                <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Apellido">
               </div>
               <br>
               <div class="form-group">
                 <label for="correo">Correo electrónico:</label>
-                <input type="email" class="form-control" id="email" placeholder="">
+                <input type="email" class="form-control" id="email" name="correo" placeholder="Correo electronico">
               </div>
               <br>
               <div class="form-group">
                 <label for="mensaje">Mensaje:</label>
-                <textarea class="form-control" id="mensaje" rows="4"></textarea>
+                <textarea class="form-control" id="mensaje" rows="4" name="mensaje" placeholder="Escribe tu mensaje"></textarea>
               </div>
               <br>
               <input type="submit" style="background-color: blue; color: white" value="Enviar">
             </div>
         </form>
+
+        <?php
+          if (isset($_GET['e'])){
+            echo "<h3> Mensaje enviado correctamente </h3>";
+          }
+        ?>
+
     </div>
     <?php include ('footer.php') ?>
 </body>
--- enviar_consulta.php
+++ enviar_consulta.php
@@ -7,3 +7,11 @@ $mensaje_form = $_POST['mensaje'];
 $cuerpo_mail = "Nombre: " .$nombre_form . "\r\n" . "Apellido: " .$apellido_form . "\r\n" . "Correo electrónico: " . "\r\n" . "Mensaje: " . $mensaje_form;
 
 mail("lucerina23@gmail.com", "Mensaje enviado desde nuestro sitio web", $cuerpo_mail);
+
+$conexion = mysqli_connect("localhost", "id19423333_bibi", ">6!u98RNa#A4<7Vw", "id19423333_cursophp") or exit ("No se pudo conectar a la base de datos");
+
+mysqli_query($conexion, "INSERT INTO consultas VALUES (DEFAULT, '$nombre_form', '$apellido_form', '$correo_form', '$mensaje_form')");
+
+mysqli_close($conexion);
+
+header("Location: contacto.php?e=ok");
