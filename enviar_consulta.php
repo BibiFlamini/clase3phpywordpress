a2a6003656fad2f77477b7a2fb40779571434444
Bibiana <lucerina23@gmail.com>
2022-09-21 15:22:09 -0300

    conectar consulta a base de datos

--- enviar_consulta.php
+++ enviar_consulta.php
@@ -8,6 +8,8 @@ $cuerpo_mail = "Nombre: " .$nombre_form . "\r\n" . "Apellido: " .$apellido_form 
 
 mail("lucerina23@gmail.com", "Mensaje enviado desde nuestro sitio web", $cuerpo_mail);
 
+// $conexion = mysqli_connect("localhost", "root", "", "bibianaflaminicursophp") or exit ("No se pudo conectar a la base de datos");
+
 $conexion = mysqli_connect("localhost", "id19423333_bibi", ">6!u98RNa#A4<7Vw", "id19423333_cursophp") or exit ("No se pudo conectar a la base de datos");
 
 mysqli_query($conexion, "INSERT INTO consultas VALUES (DEFAULT, '$nombre_form', '$apellido_form', '$correo_form', '$mensaje_form')");
